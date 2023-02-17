b90879f4cfe01bafc5b0609635130b87c6555e3c
DCardona75 <107233479+DCardona75@users.noreply.github.com>
2023-02-17 08:29:44 -0600

    push

---
+++ Mail.php
@@ -0,0 +1,59 @@
+<?php
+
+$name =$_POST['Name'];
+
+$email = $_POST['Email'];
+
+$message = $_POST['Message'];
+
+$mailheader = "From".$Name."<".$email.">\r\n";
+
+$recipient = "Cardonapro5@gmail.com";
+
+mail($recipient,$message,$mailheader)
+
+or die("ERROR");
+
+
+echo'
+
+<!DOCTYPE html>
+<html lang="en">
+    <link rel="icon" href="assets/IMG2/logo-v1.png">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with Creative Design landing page.">
+    <meta name="author" content="Devcrud">
+    <title>RGR Remodeling</title>
+
+    <!-- font icons -->
+    <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
+
+    <!-- Bootstrap + Creative Design main styles -->
+	<link rel="stylesheet" href="assets/css/creative-design.css">
+    <link rel="stylesheet" href ="assets/css/Mailmessge.css">
+
+</head>
+<body>
+    
+
+    <!-- Contact Section -->
+   <h6>
+       Thank you for Contacting us
+       <br>
+       We will get back to you soon
+    </h6>
+    
+    <div class="button">
+   <center>
+       <p class="Back">To go back to the homepage, <a href="index.html">Click Here</a>.</p>
+    </center>
+</div>
+     
+
+</body>
+</html>
+
+';
+?>
