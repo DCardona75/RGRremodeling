9d3ea69ed2e330a58d486eff04faee9123b51db7
DCardona75 <107233479+DCardona75@users.noreply.github.com>
2023-02-20 12:42:18 -0600

    added slideshow

--- assets/js/creative-design.js
+++ assets/js/creative-design.js
@@ -31,33 +31,17 @@ $(document).ready(function(){
     });
 }); 
 
-{let slideIndex = 1;
-showSlides(slideIndex);}
+let slideIndex = 0;
+showSlides();
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
