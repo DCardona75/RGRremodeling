5fd9f4e3990a0d4cddabd344a7ff475cb07d7f37
DCardona75 <107233479+DCardona75@users.noreply.github.com>
2023-03-13 08:42:39 -0500

    commit

--- Mail.php
+++ Mail.php
@@ -1,57 +1,19 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+ 
+    // recipient email address
+    $to = "rgrremodelingllc@gmail.com";
 
-$mailheader = "from:".$name."<".$email.">\r\n";
-
-$recipient = "senokicleans@gmail.com";
-
-$mail($recipient,$message,$mailheader) or die("Error");
-
-echo"Message sent";
-
-echo'
-
-<!DOCTYPE html>
-<html lang="en">
-    <link rel="icon" href="assets/IMG2/logo-v1.png">
-<head>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Start your development with Creative Design landing page.">
-    <meta name="author" content="Devcrud">
-    <title>RGR Remodeling</title>
-
-    <!-- font icons -->
-    <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
-
-    <!-- Bootstrap + Creative Design main styles -->
-	<link rel="stylesheet" href="assets/css/creative-design.css">
-    <link rel="stylesheet" href ="assets/css/Mailmessge.css">
-
-</head>
-<body>
     
+    // Do something with the data, such as sending an email
+    mail($to, "Message from $name", $message, $email);
 
-    <!-- Contact Section -->
-   <h6>
-       Thank you for Contacting us
-       <br>
-       We will get back to you soon
-    </h6>
-    
-    <div class="button">
-   <center>
-       <p class="Back">To go back to the homepage, <a href="index.html">Click Here</a>.</p>
-    </center>
-</div>
-     
+    echo 'Form submitted successfully!';
+}
 
-</body>
-</html>
-
-';
 ?>
